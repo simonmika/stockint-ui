8fe054125cfcfe9f5b8d38499d135637affc7cde
Simon Mika <simon@mika.se>
2017-10-30 14:08:28 +0100

    Fixed issues with having multiple charts.

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core"
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core"
 import * as d3 from "d3"
 
 import { DataService } from "../data.service"
@@ -6,23 +6,25 @@ import * as stockint from "stockint"
 
 @Component({
 	selector: "app-line-chart",
-	templateUrl: "./line-chart.component.html",
+	template: "<svg #svg></svg>",
 	styleUrls: ["./line-chart.component.css"],
 	providers: [DataService],
 })
 export class LineChartComponent implements OnInit {
-	transactions: stockint.Transactions
+	private transactions: stockint.Transactions
 	private margin = {top: 20, right: 20, bottom: 30, left: 50}
-	private width: number
-	private height: number
+	private width = 600
+	private height = 500
+	private get plotWidth(): number { return this.width - this.margin.left - this.margin.right }
+	private get plotHeight(): number { return this.height - this.margin.top - this.margin.bottom }
 	private x: d3.ScaleTime<number, number>
 	private y: d3.ScaleLinear<number, number>
-	private svg: d3.Selection<d3.BaseType, {}, HTMLElement, any>
+	private plot: d3.Selection<d3.BaseType, {}, HTMLElement, any>
+	@ViewChild("svg")
+	svg: ElementRef
 	constructor(private dataService: DataService) {
-		this.width = 900 - this.margin.left - this.margin.right
-		this.height = 500 - this.margin.top - this.margin.bottom
 	}
-	private getTransactions() {
+	ngOnInit() {
 		this.dataService.transactions.then(t => {
 			this.transactions = t
 			this.initSvg()
@@ -33,28 +35,27 @@ export class LineChartComponent implements OnInit {
 			this.drawLine(splitted.map(d => ({ date: d.date, value: d.low })))
 		})
 	}
-	ngOnInit() {
-		this.getTransactions()
-	}
 	private initSvg() {
-		this.svg = d3.select("svg")
+		this.plot = d3.select(this.svg.nativeElement)
+			.attr("width", this.width)
+			.attr("height", this.height)
 			.append("g")
 			.attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")")
 	}
 
 	private initAxis(d: stockint.Transactions) {
-		this.x = d3.scaleTime().range([0, this.width])
-		this.y = d3.scaleLinear().range([this.height, 0])
+		this.x = d3.scaleTime().range([0, this.plotWidth])
+		this.y = d3.scaleLinear().range([this.plotHeight, 0])
 		this.x.domain([d.date, d.endDate]).nice()
 		this.y.domain([d.low, d.high]).nice()
 	}
 
 	private drawAxis() {
-		this.svg.append("g")
+		this.plot.append("g")
 			.attr("class", "axis axis--x")
-			.attr("transform", "translate(0," + this.height + ")")
+			.attr("transform", "translate(0," + this.plotHeight + ")")
 			.call(d3.axisBottom(this.x))
-		this.svg.append("g")
+		this.plot.append("g")
 			.attr("class", "axis axis--y")
 			.call(d3.axisLeft(this.y))
 			.append("text")
@@ -70,7 +71,7 @@ export class LineChartComponent implements OnInit {
 		const line = d3.line<{date: Date, value: number}>()
 			.x(d => this.x(d.date))
 			.y(d => this.y(d.value) )
-		this.svg.append("path")
+		this.plot.append("path")
 			.datum(data)
 			.attr("class", "line")
 			.attr("fill", "none")
